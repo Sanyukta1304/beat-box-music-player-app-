2675b248f5c60875e65f8d3eb7f2df21feb071f9
Sanyukta1304 <dassanyukta9@gmail.com>
2026-03-26 19:47:53 +0530

    removed comment

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -8,4 +8,3 @@ function Album() {
 
 export default Album;
 
-//react component for the Album page, which currently just displays a heading. This component can be expanded in the future to include more functionality, such as displaying album details, tracks, and other related information.
